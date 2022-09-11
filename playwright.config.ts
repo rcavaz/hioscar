e07859fe6ad61dce8426938cd23fa6f94ee4687a
Francisco Ramos <rcavaz8f@gmail.com>
2022-09-11 11:46:56 -0700

    feat: block analytics calls for integration tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,53 +1,44 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
+import dotenv from 'dotenv';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+dotenv.config();
+if (process.env.E2E === 'true' && process.env.INTEGRATION === 'true') {
+  console.error("Mutually exclusive: Between the INTEGRATION and E2E environment variables, only one can be true");
+  process.exit(1);
+}
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+enum ScreenshotOptions {
+  ON_FAILURE = 'only-on-failure',
+  OFF = 'off',
+  ON = 'on'
+}
+const globalProjectSettings = {
+  screenshot: ScreenshotOptions.ON_FAILURE,
+};
+
 const config: PlaywrightTestConfig = {
   testDir: './tests',
-  /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
     timeout: 5000
   },
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
+        ...globalProjectSettings,
       },
     },
 
@@ -55,6 +46,7 @@ const config: PlaywrightTestConfig = {
       name: 'firefox',
       use: {
         ...devices['Desktop Firefox'],
+        ...globalProjectSettings,
       },
     },
 
@@ -62,46 +54,11 @@ const config: PlaywrightTestConfig = {
       name: 'webkit',
       use: {
         ...devices['Desktop Safari'],
+        ...globalProjectSettings,
       },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
   ],
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  outputDir: 'test-results/',
 };
 
 export default config;
--- src/pages/Home/model.ts
+++ src/pages/Home/model.ts
@@ -1,4 +1,4 @@
-import { Locator } from "@playwright/test"
+import { Locator, Page } from "@playwright/test"
 import { BasePage } from "../../../lib/basePage"
 import { selectors } from "./selectors";
 
@@ -8,6 +8,7 @@ export class HomePage extends BasePage {
     readonly url: string = 'https://www.hioscar.com';
 
     async open() {
+        await abortProductAnalyticsRequests(this.page);
         await this.page.goto(this.url);
         this.buildLocators();
     }
@@ -15,4 +16,11 @@ export class HomePage extends BasePage {
     async findDoctor() {
         await this.locators.btnFindDoctor.click();
     }
+}
+
+async function abortProductAnalyticsRequests(page: Page) {
+    await page.route('https://product-analytics.hioscar.com/v1/t', route => {
+        const request = route.request();
+        return route.abort();
+    })
 }
